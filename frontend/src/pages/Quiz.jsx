a375415df822c5509c05306f24566beda8ef0b49
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-02-12 18:18:40 -0500

    added styling to quiz

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getQuizById } from "../api/quizApi";
 import Loading from "../components/Loading";
+import "../styles/Quiz.css";
 
 function Quiz() {
   const { id } = useParams();
@@ -48,11 +49,14 @@ function Quiz() {
   if (error) return <p>{error}</p>;
   if (!quiz) return <p>Quiz not found.</p>;
   return (
-    <div>
-      <h2>{quiz.title}</h2>
-      <p>{quiz.description}</p>
+    <div className="main-container">
+      <div className="title-container">
+        <h3>{quiz.title}</h3>
+      </div>
+
       {quiz.questions && quiz.questions.length > 0 ? (
         <form
+          className="form-container"
           onSubmit={(e) => {
             e.preventDefault();
             handleSubmit();
@@ -60,33 +64,40 @@ function Quiz() {
         >
           <ul>
             {quiz.questions.map((question, questionIndex) => (
-              <li key={questionIndex}>
-                <h4>
-                  Question {questionIndex + 1}: {question.questionText}
-                </h4>
-                <ul>
-                  {question.options.map((option, optionIndex) => (
-                    <li
-                      key={optionIndex}
-                      onClick={() =>
-                        handleOptionSelect(questionIndex, optionIndex)
-                      }
-                      style={{
-                        cursor: "pointer",
-                        color:
-                          userAnswers[questionIndex] === optionIndex
-                            ? "green"
-                            : "black",
-                      }}
-                    >
-                      {option.text}
-                    </li>
-                  ))}
-                </ul>
-              </li>
+              <div className="question-container">
+                <li key={questionIndex}>
+                  <h3>
+                    Question {questionIndex + 1}: {question.questionText}
+                  </h3>
+                  <hr className="divider" />
+                  <ul>
+                    {question.options.map((option, optionIndex) => (
+                      <div className="selection">
+                        <li
+                          key={optionIndex}
+                          onClick={() =>
+                            handleOptionSelect(questionIndex, optionIndex)
+                          }
+                          style={{
+                            cursor: "pointer",
+                            color:
+                              userAnswers[questionIndex] === optionIndex
+                                ? "green"
+                                : "black",
+                          }}
+                        >
+                          <h4>{option.text}</h4>
+                        </li>
+                      </div>
+                    ))}
+                  </ul>
+                </li>
+              </div>
             ))}
           </ul>
-          <button type="submit">Submit</button>
+          <button type="submit" className="submit-btn">
+            <h3>Submit</h3>
+          </button>
         </form>
       ) : (
         <p>No questions available for this quiz.</p>
