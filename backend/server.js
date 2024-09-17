9d8e0ce984a30f201e14c2556510ab08d65d096d
sebdavid <121909326+sebs-commits@users.noreply.github.com>
2024-09-17 15:33:57 -0400

    removed deprecated option

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,6 @@ app.get("/", (req, res) => {
 app.use("/api/quizzes", quizRoutes);
 mongoose
   .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
